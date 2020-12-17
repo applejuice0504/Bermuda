94fdeccd1668f5953c75f78ea1d7c64a84088c29
applejuice0504 <zwickler.michael@googlemail.com>
2020-12-17 18:26:51 +0100

    -

---
+++ BermudaMain.java
@@ -0,0 +1,11 @@
+package wilemer.arnold.java;
+
+class BermudaMain {
+	
+		static void main(String[] args) {
+		
+		SpielFeld feld = new SpielFeld();
+		System.out.println("Y:" + feld.Y);		
+	}	
+
+}
--- Spielfeld.java
+++ Spielfeld.java
@@ -1,22 +1,41 @@
 package wilemer.arnold.java;
 
-class Spielfeld {
+class SpielFeld {
 	
 	final int X = 9;
 	final int Y = 7;
+	final int MAXSCHIFF = 4;
+	
+	private Schiff schiff[] = new Schiff[MAXSCHIFF];
 	private char feld[][] = new char[X][];
 	
-	Spielfeld() {
+	SpielFeld() {
+		
+		// Leeres Spielfeld anlegen und mit + als Wasser belegen
 		for (int x=0; x<X; x++) {
 			feld[x] = new char[Y];
 			for (int y=0; y<Y; y++) {
 				feld[x][y] = '+';
 			}
 		}
+		
+		// Schiffobjekte anlegen und im Array abspeichern
+		for (int i=0; i<MAXSCHIFF; i++) {
+			schiff[i] = new Schiff();
+		}
+				
 	}
 	
 	public char get(int x, int y){
 		return feld[x][y];
 	}
-
+	
+	public int getX() {
+		return X;
+	}
+	
+	public int getY() {
+		return Y;
+	}
+	
 }
