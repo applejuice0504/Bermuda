cc444049d88a69dbdb1f8d8b3311a13f844d1d3e
applejuice0504 <zwickler.michael@googlemail.com>
2020-12-18 12:47:07 +0100

    setPos method added to Schiff class

--- Schiff.java
+++ Schiff.java
@@ -23,6 +23,11 @@ public class Schiff {
 		this.y = y;
 	}
 	
+	public void setPos(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	public boolean isPos(int x, int y) {
 		if (this.x == x && this.y == y) 
 			return true;
